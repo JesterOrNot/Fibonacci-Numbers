6faf69e15aeddd69f6b9b4d517e0a5c9f0c1fc6f
Sean Hellum <seanhellum45@gmail.com>
2019-11-20 13:53:03 +0000

    format

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def fibonacci_seq(num1, num2, count=1):
     if count == 100:
         return
     print(num1 + num2)
-    return fibonacci_seq(num2, num1+num2,count+1)
+    return fibonacci_seq(num2, num1+num2, count+1)
+
+
 if __name__ == "__main__":
-    fibonacci_seq(0,1)
+    fibonacci_seq(0, 1)
